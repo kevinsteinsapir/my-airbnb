a0f9836c42259e03ada1a09f2746a9846156d994
Kevin Steinsapir <kevinsteinsapir@gmail.com>
2024-06-16 20:58:26 -0400

    models relations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,13 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :flat
+
+  validates :start_date, :end_date, presence: true
+  validate :end_date_after_start_date
+end
+
+def end_date_after_start_date
+  if end_date <= start_date
+    errors.add(:end_date, "must be after the start date")
+  end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,4 +1,7 @@
 class Flat < ApplicationRecord
+  has_many :bookings, dependent: :destroy
+  belongs_to :user
+
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true
